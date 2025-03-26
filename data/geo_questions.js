e360ec6e96d3a7b5a00327f449ce5151ebaccb6a
crob03 <callum03robertson@gmail.com>
2025-03-26 08:08:40 +0000

    Added geography questions data

--- data/geo_questions.js
+++ data/geo_questions.js
@@ -1,46 +1,530 @@
 export const allQuestions = [
     {
-        question: "Which is foo?",
+        question: "What is the capital of France?",
         answers: {
-            1: "Foo",
-            2: "Bar",
-            3: "FooBar",
-            4: "BarFoo"
-        },
-        correctAnswer: 1,
-        chosenAnswer: 0
-    },
-    {
-        question: "Which is bar?",
-        answers: {
-            1: "Foo",
-            2: "Bar",
-            3: "FooBar",
-            4: "BarFoo"
-        },
-        correctAnswer: 2,
-        chosenAnswer: 0
-    },
-    {
-        question: "Which is foobar?",
-        answers: {
-            1: "Foo",
-            2: "Bar",
-            3: "FooBar",
-            4: "BarFoo"
+            1: "Berlin",
+            2: "Madrid",
+            3: "Paris",
+            4: "Rome"
         },
         correctAnswer: 3,
         chosenAnswer: 0
     },
     {
-        question: "Which is barfoo?",
+        question: "Which country has the largest population?",
         answers: {
-            1: "Foo",
-            2: "Bar",
-            3: "FooBar",
-            4: "BarFoo"
+            1: "USA",
+            2: "India",
+            3: "China",
+            4: "Russia"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the longest river in the world?",
+        answers: {
+            1: "Amazon River",
+            2: "Nile River",
+            3: "Yangtze River",
+            4: "Mississippi River"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which continent is the Sahara Desert located on?",
+        answers: {
+            1: "Asia",
+            2: "Africa",
+            3: "North America",
+            4: "Australia"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country is known as the Land of the Rising Sun?",
+        answers: {
+            1: "China",
+            2: "Japan",
+            3: "South Korea",
+            4: "Thailand"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which river flows through Egypt?",
+        answers: {
+            1: "Nile River",
+            2: "Amazon River",
+            3: "Ganges River",
+            4: "Yangtze River"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country is the largest by land area?",
+        answers: {
+            1: "Canada",
+            2: "China",
+            3: "Russia",
+            4: "United States"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which mountain is the highest in the world?",
+        answers: {
+            1: "K2",
+            2: "Everest",
+            3: "Kangchenjunga",
+            4: "Makalu"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which continent is Australia part of?",
+        answers: {
+            1: "Asia",
+            2: "Europe",
+            3: "Oceania",
+            4: "Africa"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the capital of Canada?",
+        answers: {
+            1: "Toronto",
+            2: "Ottawa",
+            3: "Vancouver",
+            4: "Montreal"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country is both in Europe and Asia?",
+        answers: {
+            1: "Turkey",
+            2: "Egypt",
+            3: "Russia",
+            4: "India"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the smallest country in the world?",
+        answers: {
+            1: "Monaco",
+            2: "San Marino",
+            3: "Vatican City",
+            4: "Liechtenstein"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which ocean is the largest by area?",
+        answers: {
+            1: "Atlantic Ocean",
+            2: "Indian Ocean",
+            3: "Arctic Ocean",
+            4: "Pacific Ocean"
         },
         correctAnswer: 4,
         chosenAnswer: 0
     },
-]
+    {
+        question: "What is the largest desert in the world?",
+        answers: {
+            1: "Sahara Desert",
+            2: "Arctic Desert",
+            3: "Karakum Desert",
+            4: "Antarctic Desert"
+        },
+        correctAnswer: 4,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which city is known as the Big Apple?",
+        answers: {
+            1: "Los Angeles",
+            2: "New York City",
+            3: "Chicago",
+            4: "San Francisco"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which is the longest mountain range in the world?",
+        answers: {
+            1: "Himalayas",
+            2: "Andes",
+            3: "Rockies",
+            4: "Alps"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country has the most islands?",
+        answers: {
+            1: "Indonesia",
+            2: "Sweden",
+            3: "Norway",
+            4: "Philippines"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the capital of Italy?",
+        answers: {
+            1: "Rome",
+            2: "Milan",
+            3: "Naples",
+            4: "Venice"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which river is the longest in the United States?",
+        answers: {
+            1: "Colorado River",
+            2: "Mississippi River",
+            3: "Columbia River",
+            4: "Missouri River"
+        },
+        correctAnswer: 4,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which African country is landlocked?",
+        answers: {
+            1: "Kenya",
+            2: "Uganda",
+            3: "Egypt",
+            4: "Zimbabwe"
+        },
+        correctAnswer: 4,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the capital of Australia?",
+        answers: {
+            1: "Sydney",
+            2: "Melbourne",
+            3: "Canberra",
+            4: "Brisbane"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country is home to the ancient ruins of Machu Picchu?",
+        answers: {
+            1: "Peru",
+            2: "Mexico",
+            3: "Argentina",
+            4: "Chile"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the largest island in the world?",
+        answers: {
+            1: "Greenland",
+            2: "Australia",
+            3: "New Guinea",
+            4: "Borneo"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country does the Amazon Rainforest primarily belong to?",
+        answers: {
+            1: "Brazil",
+            2: "Peru",
+            3: "Colombia",
+            4: "Venezuela"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which ocean is the Bermuda Triangle located in?",
+        answers: {
+            1: "Atlantic Ocean",
+            2: "Indian Ocean",
+            3: "Pacific Ocean",
+            4: "Arctic Ocean"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the capital of Japan?",
+        answers: {
+            1: "Osaka",
+            2: "Kyoto",
+            3: "Tokyo",
+            4: "Hokkaido"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which U.S. state is known as the Sunshine State?",
+        answers: {
+            1: "California",
+            2: "Florida",
+            3: "Hawaii",
+            4: "Texas"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country is the largest producer of coffee?",
+        answers: {
+            1: "Colombia",
+            2: "Brazil",
+            3: "Vietnam",
+            4: "Ethiopia"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which city is home to the Eiffel Tower?",
+        answers: {
+            1: "Berlin",
+            2: "London",
+            3: "Paris",
+            4: "Rome"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which continent is the country of Egypt located in?",
+        answers: {
+            1: "Europe",
+            2: "Africa",
+            3: "Asia",
+            4: "Australia"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country is famous for its pyramids?",
+        answers: {
+            1: "Greece",
+            2: "Egypt",
+            3: "Mexico",
+            4: "Peru"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country is the Great Barrier Reef located off of?",
+        answers: {
+            1: "South Africa",
+            2: "Australia",
+            3: "Thailand",
+            4: "New Zealand"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the capital of Germany?",
+        answers: {
+            1: "Berlin",
+            2: "Munich",
+            3: "Hamburg",
+            4: "Frankfurt"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which U.S. state is known for having the Grand Canyon?",
+        answers: {
+            1: "Utah",
+            2: "Arizona",
+            3: "Nevada",
+            4: "California"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which continent is the country of Antarctica located in?",
+        answers: {
+            1: "Europe",
+            2: "Africa",
+            3: "Antarctica",
+            4: "Australia"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the capital of Spain?",
+        answers: {
+            1: "Madrid",
+            2: "Barcelona",
+            3: "Seville",
+            4: "Lisbon"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country is known as the Land of the Midnight Sun?",
+        answers: {
+            1: "Norway",
+            2: "Finland",
+            3: "Sweden",
+            4: "Iceland"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which is the second largest continent by land area?",
+        answers: {
+            1: "Africa",
+            2: "Asia",
+            3: "North America",
+            4: "South America"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country is known for having the Eiffel Tower?",
+        answers: {
+            1: "Italy",
+            2: "France",
+            3: "Spain",
+            4: "England"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which mountain range separates France from Spain?",
+        answers: {
+            1: "Andes",
+            2: "Rockies",
+            3: "Pyrenees",
+            4: "Alps"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the capital of Egypt?",
+        answers: {
+            1: "Cairo",
+            2: "Alexandria",
+            3: "Luxor",
+            4: "Giza"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which sea is bordered by Egypt, Israel, and Jordan?",
+        answers: {
+            1: "Mediterranean Sea",
+            2: "Red Sea",
+            3: "Dead Sea",
+            4: "Arabian Sea"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which is the largest country in Africa?",
+        answers: {
+            1: "Sudan",
+            2: "South Africa",
+            3: "Algeria",
+            4: "Nigeria"
+        },
+        correctAnswer: 3,
+        chosenAnswer: 0
+    },
+    {
+        question: "What is the capital of the United Kingdom?",
+        answers: {
+            1: "Edinburgh",
+            2: "London",
+            3: "Cardiff",
+            4: "Dublin"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which city is the largest in the world by population?",
+        answers: {
+            1: "Beijing",
+            2: "Tokyo",
+            3: "New York City",
+            4: "Delhi"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which U.S. state has the most volcanoes?",
+        answers: {
+            1: "Hawaii",
+            2: "Alaska",
+            3: "California",
+            4: "Washington"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country is the birthplace of pizza?",
+        answers: {
+            1: "Greece",
+            2: "Italy",
+            3: "France",
+            4: "Germany"
+        },
+        correctAnswer: 2,
+        chosenAnswer: 0
+    },
+    {
+        question: "Which country has the most time zones?",
+        answers: {
+            1: "Russia",
+            2: "United States",
+            3: "Australia",
+            4: "China"
+        },
+        correctAnswer: 1,
+        chosenAnswer: 0
+    }
+];
